eea89db23cc3363b1f637d37d2c04913139cb6bf
zd3N14 <emai.domain@com>
2024-12-26 13:31:51 +0900

    filterの作成　AuthFilterはコメントアウト中

---
+++ src/main/java/filter/AuthFilter.java
@@ -0,0 +1,53 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+
+/**
+ * Servlet Filter implementation class AuthFilter
+ */
+@WebFilter("/*")
+public class AuthFilter extends HttpFilter implements Filter {
+
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		//TODO ここにログイン認証のコード書く		
+//		HttpServletRequest req = (HttpServletRequest) request;
+//		HttpServletResponse res = (HttpServletResponse) response;
+//		HttpSession session = req.getSession();
+//		
+//		String uri = req.getRequestURI();
+//		if(!uri.endsWith("/login")) {
+//			if(session.getAttribute("loginId")== null) {
+//				res.sendRedirect("login");
+//				return;
+//			}
+//		}
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
